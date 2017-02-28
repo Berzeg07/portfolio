aca69bbaafb303b7b280ca1803208cbcb19b4fc2
engine77 <zaur.1kager@gmail.com>
2017-02-28 16:01:12 +0300

    main

--- includes/main.php
+++ includes/main.php
@@ -8,7 +8,7 @@
 				<div class="dfhover">
 					<h3>Магазин Гоблина</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
-					<a href="#" class="dfbutton">подробнее</a>
+					<a href="#" class="dfbutton">смотреть верстку</a>
 				</div>
 			</li>
 			<li class="dfliblock">
@@ -18,7 +18,7 @@
 				<div class="dfhover">
 					<h3>Мой Голос</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
-					<a href="#" class="dfbutton">подробнее</a>
+					<a href="#" class="dfbutton">смотреть верстку</a>
 				</div>
 			</li>
 			<li class="dfliblock">
@@ -28,7 +28,7 @@
 				<div class="dfhover">
 					<h3>Cafe White</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
-					<a href="#" class="dfbutton">подробнее</a>
+					<a href="#" class="dfbutton">смотреть верстку</a>
 				</div>
 			</li>
 			<li class="dfliblock">
@@ -38,7 +38,7 @@
 				<div class="dfhover">
 					<h3>Author</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
-					<a href="#" class="dfbutton">подробнее</a>
+					<a href="#" class="dfbutton">смотреть верстку</a>
 				</div>
 			</li>
 			<li class="dfliblock">
@@ -48,7 +48,7 @@
 				<div class="dfhover">
 					<h3>MPL Cargo</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
-					<a href="#" class="dfbutton">подробнее</a>
+					<a href="#" class="dfbutton">смотреть верстку</a>
 				</div>
 			</li>
 			<li class="dfliblock">
@@ -58,10 +58,70 @@
 				<div class="dfhover">
 					<h3>Твой Терем</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
-					<a href="#" class="dfbutton">подробнее</a>
+					<a href="#" class="dfbutton">смотреть верстку</a>
 				</div>
 			</li>
-	
+			<li class="dfliblock">
+				<a class="dflink" href="#">
+					<img src="img/logotornado.png" alt="" class="img-responsive">
+				</a>
+				<div class="dfhover">
+					<h3>Торнадо</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
+					<a href="#" class="dfbutton">смотреть верстку</a>
+				</div>
+			</li>
+			<li class="dfliblock">
+				<a class="dflink" href="#">
+					<img src="img/logo3.png" alt="" class="img-responsive">
+				</a>
+				<div class="dfhover">
+					<h3>Svezhaya landing</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
+					<a href="#" class="dfbutton">смотреть верстку</a>
+				</div>
+			</li>
+			<li class="dfliblock">
+				<a class="dflink" href="#">
+					<img src="img/db-header-title.png" alt="" class="img-responsive">
+				</a>
+				<div class="dfhover">
+					<h3>Кадобра</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
+					<a href="#" class="dfbutton">смотреть верстку</a>
+				</div>
+			</li>
+			<li class="dfliblock">
+				<a class="dflink" href="#">
+					<img src="img/logokaz.png" alt="" class="img-responsive">
+				</a>
+				<div class="dfhover">
+					<h3>Коллекция часов казахстан</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
+					<a href="#" class="dfbutton">смотреть верстку</a>
+				</div>
+			</li>
+			<li class="dfliblock">
+				<a class="dflink" href="#">
+					<img src="img/logocase.png" alt="" class="img-responsive">
+				</a>
+				<div class="dfhover">
+					<h3>Casenoskov</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
+					<a href="#" class="dfbutton">смотреть верстку</a>
+				</div>
+			</li>
+			<li class="dfliblock">
+				<a class="dflink" href="#">
+					<img src="img/logotouch.png" alt="" class="img-responsive">
+				</a>
+				<div class="dfhover">
+					<h3>Touch & lunch</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quibusdam.</p>
+					<a href="#" class="dfbutton">смотреть верстку</a>
+				</div>
+			</li>
+			
 		</ul>
 	</div>
 </section>
