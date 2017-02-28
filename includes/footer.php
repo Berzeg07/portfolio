52f8fd4aaf40a4a4b20867a3f1a3fc87c3f1959b
engine77 <zaur.1kager@gmail.com>
2017-02-28 15:12:42 +0300

    main

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,16 @@
-
-
-
-
-
+<div id="mask"></div>
+<a href="#log-in1" name="modal">aaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+<div class="df-popup window" id="log-in1">
+	<div class="dd-close"><img src="img/df-close.png" alt=""></div>
+	<h3 class="df-deliverycap df-gruzcap1">Напишите ваш вопрос, <br> мы ответим вам в течении суток</h3>		
+	<form method="post" class="form dfform" action="thanks.php">
+		<input type="text" placeholder="Имя" class="dfhowinp1 dfbginput" required> 
+		<input type="text" name="phone" placeholder="Email" class="phone dfhowinp2">
+		<textarea name="" placeholder="Введите сообщение" type="text"></textarea>
+		<span class="dfpopuptext">Чтобы мы могли вам ответить заполнте пожалуйста корректно поле Email*</span>
+		<input type="submit" class="button-1 button-1df button-2df" name="submit" value="Отправить">
+		
+	</form>
+</div>
 </body>
 </html>
